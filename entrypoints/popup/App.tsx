0064303be2907ac28022721456028cf69468ee09
HJSunDev <13720980943@126.com>
2024-12-05 16:48:44 +0800

    feat(ui): integrate shadcn/ui component library

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -2,14 +2,14 @@ import { useState } from 'react';
 import reactLogo from '@/assets/react.svg';
 import wxtLogo from '/wxt.svg';
 import './App.css';
+import { Button } from '@/components/ui/button';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <h1 className='bg-red-50'>hello world</h1>
+      <div className='flex items-center justify-center'>
         <a href="https://wxt.dev" target="_blank">
           <img src={wxtLogo} className="logo" alt="WXT logo" />
         </a>
@@ -17,12 +17,12 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>WXT + React</h1>
+      <h1 className='bg-blue-50'>WXT + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <Button variant="secondary" onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
-        <p>
+        </Button>
+        <p className='mt-2'>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
